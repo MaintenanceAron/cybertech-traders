2037c9b7eeb0777fe8a81108344d1eabde442bd1
Aron <kipkoecharonz@gmail.com>
2024-04-13 16:21:01 -0400

    my forex website

---
+++ assets/script.js
@@ -0,0 +1,49 @@
+/*================Sticky Navigation Bar===============*/
+
+
+
+/*================Navigation Bar Open/Close Toggle===============*/
+
+
+/*================Close Navigation Bar on Link Click===============*/
+
+
+
+/*================Testimonial Swiper initialization and configuration===============*/
+var swiper = new Swiper(".mySwiper", {
+  cssMode: true,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+  },
+  mousewheel: true,
+  keyboard: true,
+});
+
+/*================Scroll Reveal Animations===============*/
+
+
+// scroll Home
+
+
+//scroll About
+
+//why choose us
+
+
+//services
+
+
+//testimonials
+
+
+//contact
+
+
+//footer
+
